8f5135ea82ec7ab8db2df4ce99deb96a0977ea36
JongHyunJung <jjh19960115@gmail.com>
2024-09-01 10:27:12 +0900

    fix: CI 코드 수정

--- app/router/api.py
+++ app/router/api.py
@@ -4,6 +4,10 @@ from app.router.v1 import chat, heritage, image, user
 
 api_router = APIRouter()
 
+@api_router.get("/health")
+def health_check():
+    return {"status": "ok"}
+
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
 api_router.include_router(heritage.router, prefix="/heritages", tags=["heritages"])
